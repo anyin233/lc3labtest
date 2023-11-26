914d728a5e1bdbe3ead519375b5a03bc3dec4e20
cydia2001 <shizukuprprpr@gmail.com>
2023-11-26 15:46:55 +0800

    [refactor] move some shared function into seperate header

--- src/cli/labtest_single_main.cpp
+++ src/cli/labtest_single_main.cpp
@@ -1,13 +1,8 @@
 #include "lab_verifier/lab2.hpp"
-#include <bitset>
-#include <cstdint>
-#include <iterator>
 #include <ostream>
 #include <string>
-#include <fstream>
 #include <sys/types.h>
 #include <vector>
-#include <chrono>
 
 #define API_VER 2
 #include "common.h"
@@ -15,16 +10,8 @@
 #include "console_printer.h"
 #include "file_printer.h"
 #include "interface.h"
-#include "lab_verifier/verifiers.hpp"
+#include "lab_verifier/utils.hpp"
 
-struct CLIArgs {
-  uint32_t print_level = DEFAULT_PRINT_LEVEL;
-  bool enable_liberal_asm = false;
-  uint32_t id_last_num = 0;
-  std::string log_file = "";
-  std::string test_input_file = "";
-  uint32_t lab_id = 0;
-};
 
 bool endsWith(std::string const &search, std::string const &suffix) {
   if (suffix.size() > search.size()) {
@@ -33,52 +20,7 @@ bool endsWith(std::string const &search, std::string const &suffix) {
   return std::equal(suffix.rbegin(), suffix.rend(), search.rbegin());
 }
 
-std::vector<std::string> loadTestSet(const CLIArgs &args) {
-  // load test input from file
-  const std::string &test_input_file = args.test_input_file;
-  std::ifstream ifs(test_input_file);
-  if (!ifs.is_open()) {
-    std::cerr << "Error: cannot open test input file: " << test_input_file
-              << std::endl;
-    exit(1);
-  }
-  std::vector<std::string> testInput;
-  std::string line;
-  while (std::getline(ifs, line)) {
-    testInput.push_back(line);
-  }
-  ifs.close();
-  return testInput;
-}
 
-void labTestWrapper(std::string obj_filename, const CLIArgs &args) {
-  std::shared_ptr<lc3::utils::IPrinter> iprinter;
-  if (args.log_file != "") {
-    iprinter = std::make_shared<lc3::FilePrinter>(args.log_file);
-  } else {
-    iprinter = std::make_shared<lc3::ConsolePrinter>();
-  }
-
-  lc3::ConsoleInputter inputter;
-  lc3::sim simulator(*iprinter, inputter, args.print_level); 
-  auto testInput = loadTestSet(args);
-
-  int passed_count = 0;
-  switch (args.lab_id) {
-    case 1:
-      passed_count = lc3::verifier::lab1Test(simulator, obj_filename, testInput, args.id_last_num);
-      break;
-    case 2:
-      passed_count = lc3::verifier::lab2Test(simulator, obj_filename, testInput);
-      break;
-    default:
-      std::cerr << "Error: lab id " << args.lab_id << " not supported." << std::endl;
-      exit(1);
-  }
-
-  std::cout << "Passed " << passed_count << " out of " << testInput.size()
-            << " test cases." << std::endl;
-}
 
 int main(int argc, char *argv[]) {
   CLIArgs args;
